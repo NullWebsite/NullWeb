2b61c0198b3d0efed3f7b3e65ebae95e4f09a999
nullmedia-social <mjguydood@outlook.com>
2025-02-14 11:30:57 -0600

    js

--- script.js
+++ script.js
@@ -80,3 +80,13 @@ async function updateGitHubFile() {
         alert("Error: " + error.message);
     }
 }
+
+function password(pswd) {
+    let password = prompt("This is a password-protected site. Please enter the password.")
+    if (password !== pswd) {
+        alert("Incorrect password.");
+        window.location = "about:blank";
+    }
+}
+
+password("NullMediaCrew-000");
