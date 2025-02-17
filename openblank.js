eddc848e9c8c86b4ec8d99eb064c405eb4847bbe
KingNullboy <mjguydood@outlook.com>
2025-02-17 16:31:52 -0600

    js, html

---
+++ openblank.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var openBlankLink = document.getElementById('openBlankLink');
+
+    openBlankLink.addEventListener('click', function(event) {
+        event.preventDefault();
+
+        var newTab = window.open('about:blank', '_blank');
+
+        if (newTab) {
+            var newTabBody = newTab.document.body;
+            newTabBody.style.padding = '0';
+            newTabBody.style.margin = '0';
+            newTabBody.style.border = 'hidden';
+
+            var iframe = document.createElement('iframe');
+            iframe.src = window.location.href;
+            iframe.style.width = '100%';
+            iframe.style.height = '100%';
+            iframe.style.border = 'hidden';
+
+            iframe.onload = function() {
+                var links = iframe.contentDocument.querySelectorAll('a[target="_blank"]');
+                links.forEach(function(link) {
+                    link.addEventListener('click', function(event) {
+                        event.preventDefault();
+                        iframe.contentWindow.location.href = link.href;
+                    });
+                });
+            };
+
+            newTab.document.body.appendChild(iframe);
+        } else {
+            alert("couldn't manage to open a new tab :(");
+        }
+    });
+});
