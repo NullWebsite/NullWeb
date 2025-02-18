e15ab5636b83c92545c71fccf4f6bb003884f3c9
KingNullboy <mjguydood@outlook.com>
2025-02-18 10:41:18 -0600

    added nulllinks

---
+++ links/script.js
@@ -0,0 +1,19 @@
+/** Button Template:
+    <button onclick="window.location.href='urlhere';" target="_blank">namehere</button>
+**/
+
+function GxmeFiles() {
+    window.location.href='https://github.com/Nullboy000/HTMLFlashArchive/archive/refs/heads/main.zip'
+    alert('If this is your first time on this computer: After downloading the files, press the meta key (search key) type in "files", press enter, click the search bar at the top-right, type in HTMLFlashArchive, right click (click with 2 fingers) what pops up, click extract, close the files app ONCE, right click the thing that ends in ".zip", and click delete. Then click the other file twice fast, and click any file that you want to open twice fast, and enjoy your games! \n If you have done this before on this computer: Press the meta key (search key), type in files, press enter, click the search bar at the top-right, type in "HTMLFlashArchive", then click the other file twice fast, and click any file that you want to open twice fast, and enjoy your g*mes!')
+}
+
+function password(pswd) {
+    let password = prompt("This is a password-protected website. Please enter the password. While doing so, cover the text box with your hand.")
+
+    if(password != pswd) {
+        alert("Incorrect password. Please try again. Or don't!")
+        window.location = 'about:blank';
+    }
+}
+
+password("NullMediaCrew-000")
