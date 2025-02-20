42b242447ae5488f6691e16719d2618732d1b195
KingNullboy <mjguydood@outlook.com>
2025-02-20 13:40:42 -0600

    html js css

--- portraittext.js
+++ portraittext.js
@@ -1,12 +1,22 @@
 function updateVisibility() {
-    var textElement = document.getElementById("mobilePortraitText");
+    var textElement1 = document.getElementById("mobilePortraitText");
+    var textElement2 = document.getElementById("mobileOnly");
+    var textElement3 = document.getElementById("installButton");
     var isPortrait = window.matchMedia("(orientation: portrait)").matches;
     var isMobile = /Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
 
     if (isPortrait && isMobile) {
-        textElement.style = "display: block;";
+        textElement1.style = "display: block;";
     } else {
-        textElement.style = "display: none;";
+        textElement1.style = "display: none;";
+    }
+
+    if (isMobile) {
+        textElement2.style = "display: block"
+        textElement3.style = "display: block"
+    } else {
+        textElement2.style = "display: none"
+        textElement3.style = "display: none"
     }
 }
 
