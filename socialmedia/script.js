fb9f9241ef0dcf84c1da97f7b9c4fbf86564348c
KingNullboy <mjguydood@outlook.com>
2025-02-26 15:20:29 -0600

    js

--- socialmedia/script.js
+++ socialmedia/script.js
@@ -150,5 +150,19 @@ if (title.trim() === "" && postmode === undefined) {
   location.reload();
   }
 
-  
+
+}
+
+function password(pswd) {
+    let password = prompt("This is a password-protected site. Please enter the password.");
+    if (password !== pswd) {
+       alert("Incorrect password.");
+        window.location = "about:blank";
+    } else {
+        localStorage.setItem("auth", "true");
+    }
+}
+
+if (localStorage.getItem("auth") !== "true") {
+    password("NullMediaCrew-000");
 }
