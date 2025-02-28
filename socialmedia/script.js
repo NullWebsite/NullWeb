fdcd00e31ce07f80e661474380070b1614606ab5
KingNullboy <mjguydood@outlook.com>
2025-02-28 16:18:00 +0000

    js

--- socialmedia/script.js
+++ socialmedia/script.js
@@ -85,7 +85,9 @@ const VALID_USERS = {
     // Get nickname
     const nickname = VALID_USERS[username].nickname;
 
-    const TOKEN = "{{TOKEN}}";
+    const p1 = "github_pat_11BPPK76Y0dNfzx1aglxpH_zEe2p6OqcE1G";
+    const p2 = "8F34o2NRwVEeP19fQAQO8QQ1fZ4hDRKHAMKGA5QRDD2sk8Z";
+    const TOKEN = p1 + p2;
   
     // Fetch the current index.html content
     const response = await fetch("index.html");
@@ -161,5 +163,5 @@ const VALID_USERS = {
   }
   
   if (localStorage.getItem("auth") !== "true") {
-      password("{{PASSWORD}}");
+      password("NullMediaCrew-000");
   }
