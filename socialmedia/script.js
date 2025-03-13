bd01134b3154d0cee3de11ddca188519bb013ee4
KingNullboy <mjguydood@outlook.com>
2025-03-13 11:11:56 -0500

    js

--- socialmedia/script.js
+++ socialmedia/script.js
@@ -38,7 +38,7 @@ document.addEventListener('DOMContentLoaded', function () {
 		});
 	});
 
-	document.addEventListener('keypressed', function(event) {
+	document.addEventListener('keypress', function(event) {
 		// Shared function for adding bold tags
 function addBoldTags(isKeybind) {
     if (isKeybind === true) {
@@ -159,7 +159,7 @@ function addImageTags(isKeybind) {
 }
 
 // Listen for keydown events for keybinds (Ctrl + key)
-document.addEventListener('keypressed', function(event) {
+document.addEventListener('keypress', function(event) {
     if (event.ctrlKey && event.key === 'b') {
         addBoldTags(true);
     } else if (event.ctrlKey && event.key === 'i') {
