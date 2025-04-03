e625106ef16f6fae4ebd2a940614912120480b9f
KingNullboy <mjguydood@outlook.com>
2025-04-03 14:40:40 -0500

    1

--- gxmes/checknum.js
+++ gxmes/checknum.js
@@ -12,13 +12,12 @@ fetch('index.html')
       // If it's one of the matching URLs, update the "gamenum" element
       document.getElementById("gamenum").innerHTML = "There are " + buttonTimes + " g*mes of varying quality!";
     } else {
+      baseUrl = window.location.protocol + "//" + document.domain + "/links/"
+      if (currentUrl !== baseUrl || currentUrl !== baseUrl + "index" || currentUrl !== baseUrl + "index.html") {
       // Otherwise, update the "credit" and "thing" elements with the custom text
       document.getElementById("credit").innerHTML = "This g*mes website was originally made by selenite-cc on GitHub. Nullboy000 (me) added " + (buttonTimes - 50) + " more g*mes and made it an actual website, not just html files. (I think. I'm pretty sure selenite-cc wasn't using them to host a website, but given I'm basing that on the fact that I couldn't find it anywhere, I could be wrong, since I barely looked.)";
-      
+      } else {
       document.getElementById("thing").innerHTML = "NullG*mes is a website with some Flash G*mes available to play, there are currently <b>" + buttonTimes + "</b> g*mes available to play. This website was made in mind to also provide kids in schools who have free time to be able to play old g*mes they used to enjoy but have not been able to play because of Flash shutting down.";
-
-      baseUrl = window.location.protocol + "//" + document.domain + "/links/"
-        if (currentUrl === baseUrl || currentUrl === baseUrl + "index" || currentUrl === baseUrl + "index.html") {
         document.getElementById("gamenum").innerHTML = "There are " + (buttonTimes + 1) + " g*me sites/g*mes right now! However, one is temporarily out of order.";
       }
     }
