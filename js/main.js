f8a8518eeef1a4c2704060b2d4fbaa5ceadb869b
Nullboy000 <majohnson@wccbears.org>
2025-04-14 12:08:55 -0500

    js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
     if (localStorage.getItem("ads") === "true" || localStorage.getItem("ads") === null) {
       let adScript = document.createElement("script");
-      adScript.src = "//pl26379383.profitableratecpm.com/cd/95/b5/cd95b527973b743989aca62366c43fdb.js";
+      adScript.setAttribute("data-cfasync", "false");
+      adScript.setAttribute("type", "text/javascript");
+      adScript.src = "https://offfurreton.com/400/9145743";
   
       document.head.appendChild(adScript);
     }
