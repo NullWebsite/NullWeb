0c7c1d6a77740c60cc7ec61047886dc7a0a310ae
Nullboy000 <majohnson@wccbears.org>
2025-04-14 15:21:41 -0500

    js

--- js/main.js
+++ js/main.js
@@ -14,11 +14,5 @@ document.addEventListener("DOMContentLoaded", function() {
   
         document.head.appendChild(adScript);
       }, 1000);
-
-      let adScript2 = document.createElement("script");
-      adScript2.setAttribute("async", "true");
-      adScript2.src = "//d.smopy.com/d/?resource=pubJS";
-  
-      document.head.appendChild(adScript2);
     }
   });
