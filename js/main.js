61bdfad4ab4d303d2969d0e3fbe301d30b88bf49
Nullboy000 <majohnson@wccbears.org>
2025-04-14 10:55:45 -0500

    js

--- js/main.js
+++ js/main.js
@@ -1,27 +1,8 @@
-/* document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function() {
     if (localStorage.getItem("ads") === "true" || localStorage.getItem("ads") === null) {
       let adScript = document.createElement("script");
-      adScript.setAttribute("data-cfasync", "false");
-      adScript.setAttribute("type", "text/javascript");
-      adScript.src = "/adType1.js";
-      let adScript2 = document.createElement("script");
-      adScript.setAttribute("data-cfasync", "false");
-      adScript.setAttribute("type", "text/javascript");
-      adScript.src = "https://offfurreton.com/400/9145743";
+      adScript.src = "https://pl26379383.profitableratecpm.com/cd/95/b5/cd95b527973b743989aca62366c43fdb.js";
   
       document.head.appendChild(adScript);
-      document.head.appendChild(adScript2);
-  
-      let currentURL = window.location.href;
-      let baseURL = window.location.protocol + "//" + document.domain + "/";
-      let indexURLs = [baseURL, baseURL + "index", baseURL + "index.html"];
-  
-      let extraScript = document.createElement("script");
-      extraScript.setAttribute("data-cfasync", "false");
-      extraScript.setAttribute("type", "text/javascript");
-      extraScript.src = "/adType2.js";
-      extraScript.setAttribute("async", "true");
-  
-      document.head.appendChild(extraScript);
     }
-  }); */
+  });
