973c48287ace79da693e46143a25548ce698e279
Nullboy000 <majohnson@wccbears.org>
2025-04-14 12:14:16 -0500

    js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
       let adScript = document.createElement("script");
       adScript.setAttribute("data-cfasync", "false");
       adScript.setAttribute("type", "text/javascript");
-      adScript.src = "https://offfurreton.com/400/9145743";
+      adScript.src = "https://pl26379383.profitableratecpm.com/cd/95/b5/cd95b527973b743989aca62366c43fdb.js";
   
       document.head.appendChild(adScript);
     }
