71303d889d73868a482f2397e156842aff6700ad
Nullboy000 <majohnson@wccbears.org>
2025-04-15 09:13:31 -0500

    js x2

--- js/main.js
+++ js/main.js
@@ -21,5 +21,13 @@ document.addEventListener("DOMContentLoaded", function() {
       adScript2.src = "https://richinfo.co/richpartners/in-page/js/richads-ob.js?pubid=969176&siteid=361442";
   
       document.head.appendChild(adScript2);
+
+      let adScript3 = document.createElement("script");
+      adScript3.setAttribute("type", "module");
+      adScript3.setAttribute("async", "true");
+      adScript3.setAttribute("data-cfasync", "false")
+      adScript3.src = "https://richinfo.co/richpartners/push/js/rp-cl-ob.js?pubid=969176&siteid=361443&niche=33";
+  
+      document.head.appendChild(adScript3);
     }
   });
