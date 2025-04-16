778fc6e44553f876168904dd24fe6efd4cde8e60
KingNullboy <mjguydood@outlook.com>
2025-04-16 07:49:18 -0500

    .gitignore, js

--- analytics.js
+++
@@ -1,8 +0,0 @@
-//Import Mixpanel SDK
-import mixpanel from "mixpanel-browser";
-
-mixpanel.init("45db01d66f2e8e5f6e61fd4a87e6a08e", {
-  debug: true,
-  track_pageview: true,
-  persistence: "localStorage",
-});
