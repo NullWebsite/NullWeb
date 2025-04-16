70b04e23920e7d59eaab14b8005394c56c0ca98a
KingNullboy <mjguydood@outlook.com>
2025-04-16 08:51:18 -0500

    js x2

--- js/adCashAd.js
+++
@@ -1,5 +0,0 @@
-aclib.runInPagePush({
-    zoneId: '9823706',
-    refreshRate: 30,
-    maxAds: 2,
-});
--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
     if (localStorage.getItem("ads") === "true" || localStorage.getItem("ads") === null) {
-      let adLib = document.createElement("script");
-      adLib.setAttribute("id", "aclib");
-      adLib.setAttribute("type", "text/javascript");
-      adLib.src = "//acscdn.com/script/aclib.js";
+      let adScript = document.createElement("script");
+      adScript.setAttribute("data-cfasync", "false");
+      adScript.setAttribute("type", "module");
+      adScript.src = "https://richinfo.co/richpartners/push/js/rp-cl-ob.js?pubid=969176&siteid=361502&niche=33";
   
-      document.head.appendChild(adLib);
+      document.head.appendChild(adScript);
       
       setTimeout(function() {
-        let adScript = document.createElement("script");
-        adScript.setAttribute("type", "text/javascript");
-        adScript.src = "/js/adCashAd.js";
+        let adScript2 = document.createElement("script");
+        adScript2.setAttribute("async", "true");
+        adScript2.src = "https://richinfo.co/richpartners/in-page/js/richads-ob.js?pubid=969176&siteid=361500";
   
-        document.head.appendChild(adScript);
+        document.head.appendChild(adScript2);
       }, 1000);
     }
   });
