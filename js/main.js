816c6a9528d95c44faaa2b08165c5dd90f8a1d1c
KingNullboy <mjguydood@outlook.com>
2025-04-16 07:51:39 -0500

    js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     if (localStorage.getItem("ads") === "true" || localStorage.getItem("ads") === null) {
-      /* let adLib = document.createElement("script");
+      let adLib = document.createElement("script");
       adLib.setAttribute("id", "aclib");
       adLib.setAttribute("type", "text/javascript");
       adLib.src = "//acscdn.com/script/aclib.js";
@@ -13,21 +13,6 @@ document.addEventListener("DOMContentLoaded", function() {
         adScript.src = "/js/adCashAd.js";
   
         document.head.appendChild(adScript);
-      }, 1000); */
-
-      let adScript2 = document.createElement("script");
-      adScript2.setAttribute("type", "text/javascript");
-      adScript2.setAttribute("async", "true");
-      adScript2.src = "https://richinfo.co/richpartners/in-page/js/richads-ob.js?pubid=969176&siteid=361454";
-  
-      document.head.appendChild(adScript2);
-
-      let adScript3 = document.createElement("script");
-      adScript3.setAttribute("type", "module");
-      adScript3.setAttribute("async", "true");
-      adScript3.setAttribute("data-cfasync", "false")
-      adScript3.src = "https://richinfo.co/richpartners/push/js/rp-cl-ob.js?pubid=969176&siteid=361443&niche=33";
-  
-      document.head.appendChild(adScript3);
+      }, 1000);
     }
   });
