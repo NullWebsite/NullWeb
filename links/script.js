26e0427c7a9d6f89cb80ef2ddd8ee2498a6f29b1
Nullboy000 <majohnson@wccbears.org>
2025-04-21 09:23:48 -0500

    js

--- links/script.js
+++ links/script.js
@@ -1,7 +1,15 @@
-document.addEventListener("DOMContentLoaded", function() {
-function GxmeFiles() {
-    alert('This will download the source code for my website. Don\'t use NullMedia from the source code. If this is your first time downloading these files on this computer: After downloading the files, press the meta key (search key on Chromebooks) type in "files", press enter, click the search bar at the top-right, type in "NullWeb", right click (click with 2 fingers) what pops up, click extract, close the files app ONCE, right click the thing that ends in ".zip", and click delete. Then click the other file twice fast, and click any file that you want to open twice fast, and enjoy your games!\nIf you have done this before on this computer: Press the meta key (search key on Chromebooks), type in files, press enter, click the search bar at the top-right, type in "NullWeb", then click the file that pops up twice fast, and click any file that you want to open twice fast, and enjoy your g*mes!');
-    window.location.href = 'https://github.com/nullmedia-social/NullWeb/archive/refs/heads/main.zip';
+function promptPassword(pswd, altpswd) {
+    let userPassword = prompt("This is a password-protected site. Please enter the password.");
+    if (userPassword !== pswd && userPassword !== altpswd) {
+        alert("Incorrect password.");
+        window.location = "about:blank";
+    } else {
+        if (userPassword === pswd) {
+            localStorage.setItem("auth", "medialvl");
+        } else {
+            localStorage.setItem("auth", "gxmelvl");
+        }
+    }
 }
 
 async function fetchBackendPasswords() {
@@ -39,4 +47,3 @@ if (localStorage.getItem("auth") !== "medialvl" && localStorage.getItem("auth") 
         }
     });
 }
-});
