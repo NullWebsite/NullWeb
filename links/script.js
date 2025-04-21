4b807f3da6bae345379207efefb2042c95e76550
Nullboy000 <majohnson@wccbears.org>
2025-04-21 12:42:16 -0500

    js

--- links/script.js
+++ links/script.js
@@ -1,6 +1,6 @@
 function GxmeFiles() {
     alert('This will download the source code for my website. Don\'t use NullMedia from the source code. If this is your first time downloading these files on this computer: After downloading the files, press the meta key (search key on Chromebooks) type in "files", press enter, click the search bar at the top-right, type in "NullWeb", right click (click with 2 fingers) what pops up, click extract, close the files app ONCE, right click the thing that ends in ".zip", and click delete. Then click the other file twice fast, and click any file that you want to open twice fast, and enjoy your games!\nIf you have done this before on this computer: Press the meta key (search key on Chromebooks), type in files, press enter, click the search bar at the top-right, type in "NullWeb", then click the file that pops up twice fast, and click any file that you want to open twice fast, and enjoy your g*mes!');
-    window.location.href = 'https://github.com/nullmedia-social/NullWeb/archive/refs/heads/main.zip';
+    window.location.href = 'https://downgit.evecalm.com/#/home?url=https://github.com/nullmedia-social/NullWeb/tree/main/gxmes';
 }
 
 function promptPassword(pswd, altpswd) {
