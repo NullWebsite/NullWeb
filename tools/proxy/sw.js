40ccc44cf76a00ec032fda7746cc6f13af00b93b
KingNullboy <mjguydood@outlook.com>
2025-06-04 11:42:05 -0500

    Update sw.js

--- tools/proxy/sw.js
+++ tools/proxy/sw.js
@@ -1,19 +1,21 @@
 self.addEventListener('fetch', event => {
   const url = new URL(event.request.url);
+
   if (url.pathname.startsWith('/proxy/')) {
     const encoded = url.pathname.split('/proxy/')[1];
     let target;
     try {
       target = atob(encoded);
     } catch (e) {
-      event.respondWith(new Response('Bad encoding', { status: 400 }));
-      return;
+      return event.respondWith(new Response('Bad encoding', { status: 400 }));
     }
+
     event.respondWith(fetch(target, {
       method: event.request.method,
       headers: event.request.headers,
       body: event.request.body,
-      redirect: 'follow'
+      redirect: 'follow',
+      mode: 'cors',
     }));
   }
 });
